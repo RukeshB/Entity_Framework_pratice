b33e21a7fadc2ff788217f306ef3c8fd3029908e
RukeshB <rukeshbasukala@gmail.com>
2021-01-19 18:29:44 +0545

    create seprate metadata class

---
+++ EntityframeworkDotnet/EntityframeworkDotnet/Context/metadata/SizeMetadata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EntityframeworkDotnet.Context.metadata
+{
+    public partial class sizeMetadata
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Food Id")]
+        public int food_id { get; set; }
+
+        [Display(Name = "Size")]
+        public string size1 { get; set; }
+
+        [Display(Name = "Price")]
+        public int price { get; set; }
+        public bool isDeleted { get; set; }
+
+        public virtual Food Food { get; set; }
+    }
+
+}
