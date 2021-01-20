a1abdd06c63f55f91093743d89b5b552ab046129
RukeshB <rukeshbasukala@gmail.com>
2021-01-20 14:37:44 +0545

    interface and repository created

---
+++ EntityframeworkDotnet/EntityframeworkDotnet/Context/DAL/_IAllRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityframeworkDotnet.Context.DAL
+{
+    public interface _IAllRepo<T> where T : class
+    {
+        IEnumerable<T> GetModel();
+
+        T GetModelById(int modelid);
+
+        void InsertModel(T model);
+
+        void DeleteModel(int modelid);
+
+        void UpdateModel(T model);
+
+        void save();
+    }
+}
